18330811a6ce7ac6ea464f08553093509cf8ba96
Eric  Njuguna <eric@netcubetechnologies.com>
2021-05-17 14:25:48 +0300

    Reverse an Array

--- Pilot 1/Arrays and Strings/reversearray.py
+++ Pilot 1/Arrays and Strings/reversearray.py
@@ -1,2 +1,21 @@
 #A python program to reverse an Array
 
+#Function to reverse A[] from start to end
+
+def reverseList(A, start, end):
+    while start < end:
+        A[start], A[end] = A[end], A[start]
+        start += 1
+        end -= 1
+
+
+#Testing the above equation
+A = [10,5,6,9]
+print("The Original list is: ")
+print(A)
+
+#Reverse the list
+reverseList(A, 0, 3)
+
+print("The reversed list is: ")
+print(A)
